b3dbd1a9a0aa4bd718687775e8744b9d4069d2f2
Nargis <nargissamatova16@gmail.com>
2024-09-03 10:53:19 +0300

    Create a form

--- web/modules/hello_name/src/Controller/HelloNameController.php
+++ web/modules/hello_name/src/Controller/HelloNameController.php
@@ -2,13 +2,18 @@
 
 namespace Drupal\hello_name\Controller;
 
-use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Response;
+
+use Drupal\Core\Controller\ControllerBase;
+use  Drupal\Core\Link;
+use Drupal\Core\Url;
 
 class HelloNameController extends ControllerBase
 {
     public function hello()
     {
+        $url = Url::fromRoute('hello_name.form');
+        $link = Link::fromTextAndUrl($this->t('Go to the form'), $url)->toString();
         return [
             '#markup' => '<p>The simplest <b>render array</b> possible. My name is Nargis</p>',
         ];
